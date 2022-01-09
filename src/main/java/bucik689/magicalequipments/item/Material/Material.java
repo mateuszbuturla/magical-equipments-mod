035435a906e6677282b070fd3a0c2699dd0b2789
mateuszbuturla <2002mateuszbuturla@gmail.com>
2022-01-09 18:56:07 +0100

    refactor obsidian skull, ring of resistance, obsidian chunk

---
+++ src/main/java/bucik689/magicalequipments/item/Material/Material.java
@@ -0,0 +1,14 @@
+package bucik689.magicalequipments.item.Material;
+
+import bucik689.magicalequipments.item.Config;
+import dev.emi.trinkets.api.TrinketItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Material extends TrinketItem {
+
+    public Material(Settings settings, String itemName) {
+        super(settings);
+        Registry.register(Registry.ITEM, new Identifier(Config.modId, itemName), this);
+    }
+}
