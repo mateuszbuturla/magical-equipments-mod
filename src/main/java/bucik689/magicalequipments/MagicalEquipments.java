c610fcfd55dcad30681918714b406b393f4f9db0
mateuszbuturla <2002mateuszbuturla@gmail.com>
2022-01-09 11:25:44 +0100

    init commit

---
+++ src/main/java/bucik689/magicalequipments/MagicalEquipments.java
@@ -0,0 +1,12 @@
+package bucik689.magicalequipments;
+
+import bucik689.magicalequipments.item.*;
+import net.fabricmc.api.ModInitializer;
+
+public class MagicalEquipments implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/bucik689/magicalequipments/item/ModItems.java
@@ -0,0 +1,12 @@
+package bucik689.magicalequipments.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static void registerModItems() {
+    }
+}
