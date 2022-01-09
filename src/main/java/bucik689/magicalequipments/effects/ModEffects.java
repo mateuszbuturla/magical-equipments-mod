f68589d3076341c7a15e316079496bef016033dc
mateuszbuturla <2002mateuszbuturla@gmail.com>
2022-01-09 22:22:10 +0100

    added vitamis and immunity to weakness effect

--- src/main/java/bucik689/magicalequipments/effects/ModEffects.java
+++ src/main/java/bucik689/magicalequipments/effects/ModEffects.java
@@ -8,6 +8,7 @@ public class ModEffects {
 
         public static final StatusEffect ANTI_SLOWNESS = new AntiEffect(StatusEffects.SLOWNESS, "anti_slowness", 0x98D982);
         public static final StatusEffect ANTI_BLINDNESS = new AntiEffect(StatusEffects.BLINDNESS,"anti_blindness", 0x98D982);
+        public static final StatusEffect ANTI_WEAKNESS = new AntiEffect(StatusEffects.WEAKNESS, "anti_weakness", 0x98D982);
 
         public static void registerModEffects() {
         }
