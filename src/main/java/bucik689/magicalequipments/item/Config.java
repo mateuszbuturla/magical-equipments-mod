c3e3496813afb7214df9aacd39d6ae4f19b582e2
mateuszbuturla <2002mateuszbuturla@gmail.com>
2022-01-09 12:56:35 +0100

    added ring of resistance

---
+++ src/main/java/bucik689/magicalequipments/item/Config.java
@@ -0,0 +1,5 @@
+package bucik689.magicalequipments.item;
+
+public class Config {
+    public static final String modId = "magicalequipments";
+}
--- src/main/java/bucik689/magicalequipments/item/ModItems.java
+++ src/main/java/bucik689/magicalequipments/item/ModItems.java
@@ -1,11 +1,15 @@
 package bucik689.magicalequipments.item;
 
+import bucik689.magicalequipments.item.rings.RingOfResistance;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
+
+    public static final RingOfResistance RING_OF_RESISTANCE = new RingOfResistance(
+            new Item.Settings().group(ItemGroup.MISC));
 
     public static void registerModItems() {
     }
