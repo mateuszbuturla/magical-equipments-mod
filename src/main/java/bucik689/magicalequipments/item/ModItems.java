f7febb2d852a437bd9fd9d398b680f87526be20f
mateuszbuturla <2002mateuszbuturla@gmail.com>
2022-01-10 22:40:59 +0100

    refactor

--- src/main/java/bucik689/magicalequipments/item/ModItems.java
+++ src/main/java/bucik689/magicalequipments/item/ModItems.java
@@ -3,6 +3,7 @@ package bucik689.magicalequipments.item;
 import bucik689.magicalequipments.item.Material.Material;
 import bucik689.magicalequipments.item.Trinket.TrinketBase;
 import bucik689.magicalequipments.item.Trinket.Rings.RingEffect;
+import bucik689.magicalequipments.item.Trinket.Rings.RingEffectParam;
 import bucik689.magicalequipments.item.Trinket.Rings.RingEffectResistance;
 import bucik689.magicalequipments.item.Trinket.Rings.RingHealth;
 import net.minecraft.entity.effect.StatusEffects;
@@ -10,17 +11,16 @@ public class ModItems {
 
         public static final Material OBSIDIAN_CHUNK = new Material("obsidian_chunk");
 
-
-        public static final TrinketBase RING_OF_RESISTANCE = new RingEffect("ring_of_resistance", StatusEffects.RESISTANCE, 0);
-        public static final TrinketBase ADVANCED_RING_OF_RESISTANCE = new RingEffect("advanced_ring_of_resistance", StatusEffects.RESISTANCE, 1);
+        public static final TrinketBase RING_OF_RESISTANCE = new RingEffect("ring_of_resistance", new RingEffectParam[] {new RingEffectParam(StatusEffects.RESISTANCE, 0), new RingEffectParam(StatusEffects.ABSORPTION, 0)});
+        public static final TrinketBase ADVANCED_RING_OF_RESISTANCE = new RingEffect("advanced_ring_of_resistance", new RingEffectParam[] {new RingEffectParam(StatusEffects.RESISTANCE, 1)});
         public static final TrinketBase RING_OF_HEALTH = new RingHealth("ring_of_health", 2);
         public static final TrinketBase ADVANCED_RING_OF_HEALTH = new RingHealth("advanced_ring_of_health", 6);
-        public static final TrinketBase RING_OF_SPEED = new RingEffect("ring_of_speed", StatusEffects.SPEED, 0);
-        public static final TrinketBase ADVANCED_RING_OF_SPEED = new RingEffect("advanced_ring_of_speed", StatusEffects.SPEED, 1);
-        public static final TrinketBase RING_OF_STRENGTH = new RingEffect("ring_of_strength", StatusEffects.STRENGTH, 0);
-        public static final TrinketBase ADVANCED_RING_OF_STRENGTH = new RingEffect("advanced_ring_of_strength", StatusEffects.STRENGTH, 1);
+        public static final TrinketBase RING_OF_SPEED = new RingEffect("ring_of_speed", new RingEffectParam[] {new RingEffectParam(StatusEffects.SPEED, 0)});
+        public static final TrinketBase ADVANCED_RING_OF_SPEED = new RingEffect("advanced_ring_of_speed", new RingEffectParam[] {new RingEffectParam(StatusEffects.SPEED, 1)});
+        public static final TrinketBase RING_OF_STRENGTH = new RingEffect("ring_of_strength", new RingEffectParam[] {new RingEffectParam(StatusEffects.STRENGTH, 0)});
+        public static final TrinketBase ADVANCED_RING_OF_STRENGTH = new RingEffect("advanced_ring_of_strength", new RingEffectParam[] {new RingEffectParam(StatusEffects.STRENGTH, 1)});
 
-        public static final TrinketBase OBSIDIAN_SKULL = new RingEffect("obsidian_skull", StatusEffects.FIRE_RESISTANCE, 0);
+        public static final TrinketBase OBSIDIAN_SKULL = new RingEffect("obsidian_skull", new RingEffectParam[] {new RingEffectParam(StatusEffects.FIRE_RESISTANCE, 0)});
         public static final TrinketBase FAST_CLOCK = new RingEffectResistance("fast_clock", StatusEffects.SLOWNESS);
         public static final TrinketBase BLINDFOLD = new RingEffectResistance("blindfold", StatusEffects.BLINDNESS);
         public static final TrinketBase ADHESIVE_BANDAGE = new RingEffectResistance("adhesive_bandage", StatusEffects.WITHER);
@@ -35,5 +35,6 @@ public class ModItems {
         public static final TrinketBase BEZOAR = new RingEffectResistance("bezoar", StatusEffects.POISON);
 
         public static void registerModItems() {
+                
         }
 }
--- src/main/java/bucik689/magicalequipments/item/Trinket/Rings/RingEffect.java
+++ src/main/java/bucik689/magicalequipments/item/Trinket/Rings/RingEffect.java
@@ -3,36 +3,39 @@ package bucik689.magicalequipments.item.Trinket.Rings;
 import bucik689.magicalequipments.item.Trinket.TrinketBase;
 import dev.emi.trinkets.api.SlotReference;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.ItemStack;
 
 public class RingEffect extends TrinketBase {
 
-    private final StatusEffect effect;
-    private final int amplifier;
+    private RingEffectParam[] effects;
 
-    public RingEffect(String name, StatusEffect effect, int amplifier) {
+    public RingEffect(String name, RingEffectParam[] effects) {
         super(name);
-        this.effect = effect;
-        this.amplifier = amplifier;
+        this.effects = effects;
     }
 
     @Override
     public void onEquip(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        if (entity.hasStatusEffect(effect)) {
-            return;
+        if (effects.length > 0) {
+            for (int i = 0; i < effects.length; i++) {
+                if (!entity.hasStatusEffect(effects[i].effect)) {
+                    StatusEffectInstance effectInstance = new StatusEffectInstance(effects[i].effect, 160,
+                            effects[i].amplifier, false, false);
+                    entity.addStatusEffect(effectInstance);
+                }
+            }
         }
-        StatusEffectInstance effectInstance = new StatusEffectInstance(effect, 160, amplifier, false, false);
-        entity.addStatusEffect(effectInstance);
     }
 
     @Override
     public void onUnequip(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        if (!entity.hasStatusEffect(effect)) {
-            return;
+        if (effects.length > 0) {
+            for (int i = 0; i < effects.length; i++) {
+                if (entity.hasStatusEffect(effects[i].effect)) {
+                    entity.removeStatusEffect(effects[i].effect);
+                }
+            }
         }
-        entity.removeStatusEffect(effect);
     }
-
 }
