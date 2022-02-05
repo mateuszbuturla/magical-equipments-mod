be37e506f03dbbadec987a3ae0b48380a3177bb6
mateuszbuturla <2002mateuszbuturla@gmail.com>
2022-02-05 22:34:30 +0100

    fixed health rings

--- src/main/java/net/bucik689/magicalequipment/item/Trinket/Ring/RingHealth.java
+++ src/main/java/net/bucik689/magicalequipment/item/Trinket/Ring/RingHealth.java
@@ -1,15 +1,23 @@
 package net.bucik689.magicalequipment.item.Trinket.Ring;
 
+import java.util.UUID;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
 import net.bucik689.magicalequipment.item.Rarrity;
 import net.bucik689.magicalequipment.item.Trinket.SlotType;
 import net.bucik689.magicalequipment.item.Trinket.TrinketBase;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 
 public class RingHealth extends TrinketBase implements ICurioItem {
+
+    private static final UUID MAX_HEALTH_UUID = UUID.fromString("da715194-1f3e-47df-9d02-f66d2f553a4d");
 
     private final int healthAmount;
 
@@ -19,17 +27,13 @@ public class RingHealth extends TrinketBase implements ICurioItem {
     }
 
     @Override
-    public void onEquip(SlotContext slotContext, ItemStack prevStack, ItemStack stack) {
-        slotContext.getWearer().getAttribute(Attributes.MAX_HEALTH).setBaseValue(20 + healthAmount);
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext,
+            UUID uuid, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> modifiers = HashMultimap.create();
+        modifiers.put(Attributes.MAX_HEALTH,
+                new AttributeModifier(MAX_HEALTH_UUID, "Max Health", healthAmount,
+                        AttributeModifier.Operation.ADDITION));
 
-        onEquip(slotContext.identifier(), slotContext.index(), slotContext.entity(), stack);
-    }
-
-    @Override
-    public void onUnequip(SlotContext slotContext, ItemStack newStack, ItemStack stack) {
-        LivingEntity livingEntity = slotContext.getWearer();
-        livingEntity.getAttribute(Attributes.MAX_HEALTH)
-                .setBaseValue(livingEntity.getAttributeValue(Attributes.MAX_HEALTH) - healthAmount);
-        onUnequip(slotContext.identifier(), slotContext.index(), slotContext.entity(), stack);
+        return modifiers;
     }
 }
