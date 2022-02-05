079466d5eac476b0bacda1446a6c15e76075fe4f
mateuszbuturla <2002mateuszbuturla@gmail.com>
2022-02-05 16:36:43 +0100

    added mythril sword

--- src/main/java/net/bucik689/magicalequipment/item/Tool/BaseSword.java
+++ src/main/java/net/bucik689/magicalequipment/item/Tool/BaseSword.java
@@ -2,15 +2,14 @@ package net.bucik689.magicalequipment.item.Tool;
 
 import net.bucik689.magicalequipment.item.ModCreativeModeTab;
 import net.bucik689.magicalequipment.item.ModItems;
-import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.Item;
-
+import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.common.ForgeTier;
 
-public class BaseAxe extends AxeItem {
+public class BaseSword extends SwordItem {
 
-    public BaseAxe(String itemName, ForgeTier tier) {
-        super(tier, 2F, -3F,
+    public BaseSword(String itemName, ForgeTier tier) {
+        super(tier, 0, -2.4F,
                 new Item.Properties().tab(ModCreativeModeTab.MAGICAL_EQUIPMENT_TAB));
         ModItems.ITEMS.register(itemName, () -> this);
     }
